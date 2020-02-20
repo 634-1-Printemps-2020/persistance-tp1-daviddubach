194e43121c6467bdaba7beaa2160344b554f05e3
daviddubach <55696488+daviddubach@users.noreply.github.com>
2020-02-20 12:23:41 +0100

    Update Main.java

--- src/test/java/ch/hesge/cours634/counter/Main.java
+++ src/test/java/ch/hesge/cours634/counter/Main.java
@@ -24,6 +24,5 @@ public class Main {
         System.out.println(cLimited.getValue() + " (add 4)");
         cLimited.add(3);
         System.out.println(cLimited.getValue() + " (add 3)");
-
     }
 }
